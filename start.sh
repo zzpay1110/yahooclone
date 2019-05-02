3ef2d4b9ba0548ac07378fbb4b423eff282def4f
zzpay1110 <50121531+zzpay1110@users.noreply.github.com>
2019-05-02 18:49:13 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+clear
+echo "";
+echo "#######################################################○○○○○#○○○○○#○○○####○###○###○#########################○#####○##○##○##○#○###○#○#########################○#####○###○○○##○○○○○###○####YAHOO###############○○○○○#○○○○○#○####○###○###○####CLONE############################################################";
+sleep 5
+echo "**********WELLCOME TO YAHOO CLONE @ZzpaY**********";
+echo "##################################################";
+echo "BACA DULU BOS!!!!!!!";
+sleep 3
+echo "";
+echo "1. login dulu akun FB anda menggunakan FB Lite atau Browser bawaan hp anda untuk menghindari akun kena CHECK POIN";
+sleep 2
+echo "2. Gunakan dengan BIJAK!!!!";
+read -p "TEKAN Enter Jika SETUJU!!!!!" a;
+echo "》》》》》LOADING YAHOOCLONE《《《《《";
+sleep 3
+echo "0% load config.cfg";
+sleep 3
+echo "10% load html.dll*********";
+sleep 5
+echo "25% load sql.dll*******************";
+sleep 7
+echo "75% load yahoo.inf***************************";
+sleep 5
+echo "100% load complited*******************************";
+sleep 5
+echo  "ALL OK!";
+sleep 5
+python2 start.py
---
+++ yahooclone.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+clear
+echo "";
+read -p "@Yahooclone******Silahkan Enter Untuk Menginstall Bahan :) " a;
+sleep 2
+echo " *******Installer by @zzpay*******"
+sleep 2
+echo "[+] Tunggu Sebentar Sedang Menginstall PYTHON2...";
+pkg install python2
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall GIT...";
+pkg install git
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall REQUESTS...";
+pip2 install requests
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall MECHANIZE...";
+pip2 install mechanize
+sleep 1
+echo "[+] Tunggu Sebentar Sedang Menginstall YAHOOCLONE...";
+unzip Yahooclone.zip -o
+sleep 1
+echo "[+] Installing...";
+sleep 2
+echo "";
+echo "[+] Install Selesai...";
+sleep
+sh start.sh
